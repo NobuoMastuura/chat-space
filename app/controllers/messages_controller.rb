055043c1db52e003b700df323a998162383c5f00
Nobuo <akiyamajyoni0922@gmail.com>
2018-03-30 15:39:17 +0900

    投稿機能テストコード

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
   def index
   	@message = Message.new
   	@messages = @group.messages.includes(:user)
-    @groups = @group.users 
+    @groups = @group.users
   end
 
   def create
@@ -22,13 +22,10 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-  	params.require(:message).permit(:content, :image).merge(user_id: current_user.id)  	
+  	params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
   def set_group
   	@group = Group.find(params[:group_id])
   end
 end
-
-
-
---
+++ spec/factories/messages.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :message do
+    content Faker::Lorem.sentence
+    image File.open("#{Rails.root}/public/uploads/message/image/26/48504164-D7F2-4E59-A1E3-43A4DD53858A.jpg")
+    user
+    group
+  end
+end

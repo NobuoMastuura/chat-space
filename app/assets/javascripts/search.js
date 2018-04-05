41cc8e122c51f7beaf66917d61f6763df941f98b
Nobuo <akiyamajyoni0922@gmail.com>
2018-04-05 15:01:14 +0900

    Fix search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,44 @@
 $(function(){
+
+var search_list = $('#user-search-result');
+
+function appendGroup(group){
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name"> ${ group.name } </p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ group.id }" data-user-name="${ group.name }">追加</a>
+              </div>`
+  search_list.append(html);
+}
+
+function appendNoGroup(group){
+  var html = `<div class="chat-group-user clearfix">
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add">${ group }</a>
+              </div>`
+  search_list.append(html);
+}
+
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
-    console.log(input);
+    console.log(input)
+    $.ajax({
+      type: 'GET',
+      url: '/groups/search',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(groups){
+      $('#user-search-result').empty();
+      if (groups.length !== 0){
+        groups.forEach(function(group){
+          appendGroup(group);
+        });
+      }
+      else {
+        appendNoGroup("一致するユーザーはいません")
+      }
+    })
+    .fail(function(){
+      alert('error');
+    })
   });
 });

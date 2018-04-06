fc35a8682e04b333214dee7fd08a51c4d8fedb9d
Nobuo <akiyamajyoni0922@gmail.com>
2018-04-06 17:43:02 +0900

    Fix bug

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -5,55 +5,62 @@ var search_list = $('#user-search-result');
 var result_list = $('#chat-group-users');
 
 // インクリメンタルサーチに引っかかった時
-function appendGroup(group){
+function appendUser(user, current_user){
   var html = `<div class="chat-group-user clearfix">
-                <p class="chat-group-user__name"> ${ group.name } </p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ group.id }" data-user-name="${ group.name }">追加</a>
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
               </div>`
-  search_list.append(html);
+  if (user.id != current_user) {
+      search_list.append(html);
+  }
 }
 // インクリメンタルサーチに引っかからなかった時
-function appendNoGroup(group){
+function appendNoUser(user){
   var html = `<div class="chat-group-user clearfix">
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add">${ group }</a>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add">${user}</a>
               </div>`
   search_list.append(html);
 }
 // メンバーを追加する機能
 function appendGroupMember(name, id){
-  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ id }'>
-                <input name='group[user_ids][]' type='hidden' value='${ id }'>
-                <p class='chat-group-user__name'> ${ name } </p>
+  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
+                <input name='group[user_ids][]' type='hidden' value='${id}'>
+                <p class='chat-group-user__name'> ${name} </p>
                 <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
               </div>`
   result_list.append(html);
 }
   $("#user-search-field").on("keyup", function(){
+    // if文を使って空の状態のときだけajax通信を行う
+    var none = ""
     var input = $("#user-search-field").val();
-    console.log(input)
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-    .done(function(groups){
-      $('#user-search-result').empty();
-      if (groups.length !== 0){
-        groups.forEach(function(group){
-          appendGroup(group);
-        });
-      }
-      else {
-        appendNoGroup("一致するユーザーはいません")
-      }
-    })
-    .fail(function(){
-      alert('error');
-    })
+    var current_user = $(".current_user_name").val();
+    // 空白じゃないときだけajax通信を行う
+    if (input != none) {
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
+      })
+      .done(function(users){
+        $('#user-search-result').empty();
+        if (users.length !== 0){
+          users.forEach(function(user){
+            appendUser(user, current_user);
+          });
+        }
+        else {
+          appendNoUser("一致するユーザーはいません")
+        }
+      })
+      .fail(function(){
+        alert('error');
+      })
+    }
   });
   // インクリメンタルサーチででてきたユーザーを「チャットメンバー」のボックスに追加するため
-  $("#user-search-result").on("click", "a",  function(){
+  $("#user-search-result").on("click", ".user-search-add.chat-group-user__btn.chat-group-user__btn--add",  function(){
     var id = $(this).attr('data-user-id');
     var name = $(this).attr('data-user-name');
     // emptyは要素の子要素を消し去る
@@ -62,7 +69,7 @@ function appendGroupMember(name, id){
     appendGroupMember(name, id)
     })
   // インクリメンタルサーチで追加したユーザーを削除するためのもの
-    $("#chat-group-users").on("click", "a", function(){
-      $(this).parent().empty();
+    $("#chat-group-users").on("click", ".user-search-remove.chat-group-user__btn.chat-group-user__btn--remove.js-remove-btn", function(){
+      $(this).parent().remove();
     })
 });

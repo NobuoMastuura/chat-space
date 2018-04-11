742d1e615f6ff02e2edc650aafe92f16836e229e
Nobuo <akiyamajyoni0922@gmail.com>
2018-04-11 13:04:07 +0900

    【Fix】　部分修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_group
-
+	before_action :set_group
 	def index
 		@message = Message.new
 		@messages = @group.messages.includes(:user)
@@ -12,8 +11,8 @@ class MessagesController < ApplicationController
 		end
 	end
 
-  def create
-    @message = @group.messages.new(message_params)
+	def create
+　　@message = @group.messages.new(message_params)
 		if @message.save
 			respond_to do |format|
 				format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}

9e9df972968d9b8a059f568aefc5158284c39691
Nobuo <akiyamajyoni0922@gmail.com>
2018-04-12 16:40:43 +0900

    fix

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/uploads/message/image/26/48504164-D7F2-4E59-A1E3-43A4DD53858A.jpg")
+    image File.open("#{Rails.root}/public/uploads/message/image/35/B45236A6-FA0C-45A7-8D6F-F391CD538394.JPG")
     user
     group
   end
